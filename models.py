594a83764d69703becacac9f4bd614685062d81d
vladimir <m9043887765@gmail.com>
2023-10-13 17:59:12 +0500

    ggg

--- models.py
+++ models.py
@@ -5,11 +5,11 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_USER = os.getenv("DB_USER", "vovman")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Topol13")
-DB_NAME = os.getenv("DB_NAME", "DB_AIOHTTP")
-DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
-DB_PORT = os.getenv("DB_PORT", "5431")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
 PG_DSN = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
